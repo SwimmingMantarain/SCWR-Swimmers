40e0e3d95e0cca57edf2f0787cf5527efdff7e22
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-07-28 13:05:55 +0200

    adding swimmers

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Form, Request, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import Union, Annotated
-from swimrankings import get_scwr_swimmers
+import swimrankings
 
 router = APIRouter(prefix="/v1")
 
@@ -26,13 +26,29 @@ CREATE TABLE IF NOT EXISTS swimmers (
 cursor.execute(query) 
 
 @router.post("/add-swimmer", response_class=HTMLResponse)
-async def api_add_swimmer(request: Request, full_name: str = Form(...), hx_request: Annotated[Union[str, None], Header()] = None):
+async def api_add_swimmer(request: Request, full_name: Annotated[Union[str, None], Header(alias="HX-Prompt")] = None, hx_request: Annotated[Union[str, None], Header(alias="HX-Request")] = None):
     if hx_request:
-        print(full_name)
+        swimmer = swimrankings.get_swimmer(full_name)
+
+        if swimmer:
+            query = """INSERT INTO swimmers(sw_id, birth_year, first_name, last_name)
+                       VALUES(?, ?, ?, ?);"""
+            cursor.execute(query, (swimmer[0], swimmer[1], swimmer[2], swimmer[3]))
+
+            db.commit()
+
+            query = "SELECT * FROM swimmers"
+            cursor.execute(query)
+            swimmers = cursor.fetchall()
+            return templates.TemplateResponse(
+                request=request, name="htmx/admin_view_db.html", context = {"swimmers": swimmers}
+            )
+
+
 
 @router.post("/sync-swimmers", response_class=HTMLResponse)
 async def api_sync_swimmers(request: Request, hx_request: Annotated[Union[str, None], Header()] = None):
-    swimmers = get_scwr_swimmers()
+    swimmers = swimrankings.get_scwr_swimmers()
     sw_ids = [swimmer[0] for swimmer in swimmers]
     for swimmer in swimmers:
         query = "SELECT * FROM swimmers WHERE sw_id = ?"
@@ -40,7 +56,7 @@ async def api_sync_swimmers(request: Request, hx_request: Annotated[Union[str, N
         db_swimmer = cursor.fetchall()
         if not db_swimmer:
             query = """INSERT INTO swimmers(sw_id, birth_year, first_name, last_name)
-                        VALUES(?, ?, ?, ?);"""
+                       VALUES(?, ?, ?, ?);"""
             cursor.execute(query, (swimmer[0], swimmer[1], swimmer[2], swimmer[3]))
 
     # disgusting
--- swimrankings.py
+++ swimrankings.py
@@ -31,3 +31,30 @@ def get_scwr_swimmers():
 
     else:
         print(f"Failed to fetch SCWR athletes: {response.status_code}")
+
+def get_swimmer(full_name: str):
+    first_name, last_name = full_name.split(', ')
+    url = f'https://www.swimrankings.net/index.php?&internalRequest=athleteFind&athlete_clubId=-1&athlete_gender=-1&athlete_lastname={last_name}&athlete_firstname={first_name}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find("table", attrs={'class': 'athleteSearch'})
+        row = table.find('tr', attrs={'class': 'athleteSearch0'})
+
+        td_name = row.find('td', attrs={'class': 'name'})
+        td_date = row.find('td', attrs={'class': 'date'})
+
+        birth_year = int(td_date.get_text())
+
+        a_name = td_name.find('a')
+        last_name, first_name = a_name.get_text().split(', ')
+        last_name.capitalize()
+
+        a_href = a_name['href']
+        athlete_id = int(a_href[30:37])
+
+        return [athlete_id, birth_year, first_name, last_name]
+
+    else:
+        print(f"Failed to fetch Athlete {full_name}: {response.status_code}")
