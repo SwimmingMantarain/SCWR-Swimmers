a56e8eff036fd038507e44354dddac497ef2a3a6
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-08-07 21:41:43 +0200

    added qol run_server.sh bash file :)

---
+++ run_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+fastapi dev app.py --host 0.0.0.0 &
+PID=$!
+
+if command -v xdg-open > /dev/null; then
+		xdg-open http://localhost:8000/
+elif command -v open > /dev/null; then
+		open http://localhost:8000/
+else
+		echo "Please manually open http://localhost:8000/"
+fi
+
+trap 'echo "Interrupted. Stopping Fastapi Server..."; kill $PID; exit' INT
+
+wait $PID
