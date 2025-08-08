83933d70cdc13ee8ba555ca1fd4ddce62b2497d3
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-08-08 21:57:54 +0200

    added more secure tokens

--- admin.py
+++ admin.py
@@ -2,24 +2,38 @@ from fastapi import APIRouter, Request, Header, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from typing import Annotated, Union
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 from api import db
 import bcrypt
+import secrets
 import os
 
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+load_dotenv()
+pw = os.getenv("PASSWORD").encode()
 
-load_dotenv();
-pw = os.getenv("PASSWORD").encode();
+TOKEN_LIFETIME = timedelta(hours=1)
+active_tokens = {} # token: expiry
 
+def verify_token(token: str | None) -> bool:
+    if not token: return False
+    expiry = active_tokens.get(token)
+    if not expiry:
+        return False
+    if datetime.now(timezone.utc) > expiry:
+        del active_tokens[token]
+        return False
+    return True
 
 @router.post("/admin", response_class=HTMLResponse)
 async def admin_login_post(request: Request, password: str = Form(...)):
     if bcrypt.checkpw(password.encode('utf-8'), pw):
-        token = 'secure'
+        token = secrets.token_urlsafe(128);
+        active_tokens[token] = datetime.now(timezone.utc) + TOKEN_LIFETIME
         response = templates.TemplateResponse(
             request=request, name="admin/dashboard.html"
         )
@@ -34,7 +48,7 @@ async def admin_login_post(request: Request, password: str = Form(...)):
 async def admin_login(request: Request):
     token = request.cookies.get("access_token")
 
-    if token == "secure":
+    if verify_token(token):
         return templates.TemplateResponse(
             request=request, name="htmx/dashboard.html"
         )
@@ -47,7 +61,7 @@ async def admin_view_db(request: Request, hx_request: Annotated[Union[str, None]
     token = request.cookies.get("access_token")
 
     if hx_request:
-        if token == "secure":
+        if verify_token(token):
             swimmers = db.get_all_from('scwr_swimmers')
             return templates.TemplateResponse(
                 request=request, name="htmx/admin_view_db.html", context = {"swimmers": swimmers}
@@ -59,7 +73,7 @@ async def admin_view_db(request: Request, hx_request: Annotated[Union[str, None]
             response.headers["HX-Push-Url"] = "/admin"
             return response
     else:
-        if token == "secure":
+        if verify_token(token):
             swimmers = db.get_all_from('scwr_swimmers')
             return templates.TemplateResponse(
                 request=request, name="admin/view_db.html", context = {"swimmers": swimmers}
