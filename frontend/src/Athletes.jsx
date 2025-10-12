7da410cca39a14d794edf2a6e9f6234247bf675b
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-10-12 16:28:50 +0200

    poop: secret

--- frontend/src/Athletes.jsx
+++ frontend/src/Athletes.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+
 function Athletes() {
+    const api_key = import.meta.env.VITE_APIKEY;
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/v1/athletes', {
+            headers: {
+                'x-api-key': api_key
+            }
+        })
+            .then(res => res.json())
+            .then(json => setData(json));
+    }, []);
+
     return (
         <div id="content">
-            <p>Poop</p>
+            <p>Poop: {api_key}</p>
+            {data ? JSON.stringify(data) : "Pooping..."}
         </div>
     )
 }
--- run_dev.sh
+++ run_dev.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 fastapi dev app.py --host 0.0.0.0 &
-PID=$!
+BACK_PID=$!
 
-sleep 3s
+cd frontend
+npm run dev -- --host 0.0.0.0 &
+FRONT_PID=$!
+
+sleep 5s
 
 if command -v xdg-open > /dev/null; then
-		xdg-open http://localhost:8000/
+    xdg-open http://localhost:5173/
 elif command -v open > /dev/null; then
-		open http://localhost:8000/
+    open http://localhost:5173/
 else
-		echo "Please manually open http://localhost:8000/"
+    echo "Please manually open http://localhost:5173/"
 fi
 
-trap 'echo "Interrupted. Stopping Fastapi Server..."; kill $PID; wait $PID 2>/dev/null; clear; exit' INT
+trap 'clear; 
+echo "Interrupted. Stopping Fastapi Server...";
+kill $BACK_PID; wait $BACK_PID 2>/dev/null;
+kill $FRONT_PID; wait $FRONT_PID 2>/dev/null;
+clear; exit' INT
 
-wait $PID
+wait
