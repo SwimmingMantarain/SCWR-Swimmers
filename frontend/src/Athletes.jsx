951c4e5f1640a63e92a539c878719f5999f1a0a7
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-10-12 18:01:02 +0200

    no pooping :(

--- frontend/src/Athletes.jsx
+++ frontend/src/Athletes.jsx
@@ -1,24 +1,41 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Athletes() {
     const api_key = import.meta.env.VITE_APIKEY;
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/v1/athletes', {
-            headers: {
-                'x-api-key': api_key
+        async function fetchAthletes() {
+            try {
+                const reponse = await axios.get("http://localhost:8000/v1/athletes", {
+                    headers: {
+                        'x-api-key': api_key
+                    }
+                });
+                setData(response.data);
+            } catch (err) {
+                setError("Failed to fetch athletes");
+            } finally {
+                setLoading(false);
             }
-        })
-            .then(res => res.json())
-            .then(json => setData(json));
+        }
     }, []);
+
+    if (loading) return <div id="content">Loading...</div>;
+    if (error) return <div id="content">{error}</div>;
 
     return (
         <div id="content">
-            <p>Poop: {api_key}</p>
-            {data ? JSON.stringify(data) : "Pooping..."}
+            <h1>Athletes</h1>
+            <ul>
+                {data.map((athlete) => (
+                    <li key={athlete.id}>{athlete.first_name} {athlete.last_name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
